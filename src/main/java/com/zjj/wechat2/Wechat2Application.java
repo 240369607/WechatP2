2c52287bc1f0c0df64f396d1e2ad273333e86a48
DELL <DELL@hello>
2019-03-28 20:12:45 +0800

    初次提交

---
+++ src/main/java/com/zjj/wechat2/Wechat2Application.java
@@ -0,0 +1,13 @@
+package com.zjj.wechat2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Wechat2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Wechat2Application.class, args);
+	}
+
+}
